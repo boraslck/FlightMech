e04fd5ce267355ec66476a4d8c387c6292e96d20
boraslck <bsel0779@uni.sydney.edu.au>
2022-10-14 10:29:41 +1100

    init

---
+++ Functions/Initilisation.m
@@ -0,0 +1,9 @@
+%% Initialisation
+
+%%
+function [Nominal, Secondary] = initilisation
+
+Nominal         = aero3560_LoadFlightDataPC9_CG2;
+Secondary       = aero3560_LoadFlightDataPC9_nominalCG1;
+
+end
