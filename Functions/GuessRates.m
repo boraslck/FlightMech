512588ce28e91b8c2383982b26475c7da8d1a4a0
boraslck <bsel0779@uni.sydney.edu.au>
2022-10-18 16:30:35 +1100

    f

--- Functions/GuessRates.m
+++ Functions/GuessRates.m
@@ -7,20 +7,21 @@ alpha_zero = 0;
 beta_zero = 0; 
 
 % Iteration parameters
-error = 1;
+error_abs = 1;
 toler = 1e-9;
 IterLim = 100;
 IterCount = 0;
 
-while error > toler
+while error_abs > toler
    
     angle_rates = [alpha_zero, beta_zero];
+    
     [Xdot CL Cy] = StateRates (FlightData, X, U, angle_rates);
     
     [alpha_dot, beta_dot] = AngularRates(Xdot,X);
     
     error_alp = abs((alpha_dot - alpha_zero)/ alpha_zero);
-    error_bet = abs((alpha_dot - alpha_zero)/ alpha_zero);
+    error_bet = abs((beta_dot - beta_zero)/ beta_zero);
     error_abs = max([error_alp error_bet]);
     
     alpha_zero = alpha_dot;
@@ -30,7 +31,7 @@ while error > toler
         fprintf('Guess Rates Iteration Limit Error')
         break
     end
-    
+    angle_rates
     IterCount = IterCount + 1;
     
     
--- Functions/e2q.m
+++ Functions/e2q.m
@@ -18,10 +18,10 @@ function [quaternion] = e2q(eulers)
     end
     
     %% Quaternion expressions with euler angle inputs
-    q0 = cosd(e3/2)*cosd(e2/2)*cosd(e1/2) + sind(e3/2)*sind(e2/2)*sind(e1/2);
-    q1 = cosd(e3/2)*cosd(e2/2)*sind(e1/2) - sind(e3/2)*sind(e2/2)*cosd(e1/2);
-    q2 = cosd(e3/2)*sind(e2/2)*cosd(e1/2) + sind(e3/2)*cosd(e2/2)*sind(e1/2);
-    q3 = -cosd(e3/2)*sind(e2/2)*sind(e1/2) + sind(e3/2)*cosd(e2/2)*cosd(e1/2);
+    q0 = cos(e3/2)*cos(e2/2)*cos(e1/2) + sin(e3/2)*sin(e2/2)*sin(e1/2);
+    q1 = cos(e3/2)*cos(e2/2)*sin(e1/2) - sin(e3/2)*sin(e2/2)*cos(e1/2);
+    q2 = cos(e3/2)*sin(e2/2)*cos(e1/2) + sin(e3/2)*cos(e2/2)*sin(e1/2);
+    q3 = -cos(e3/2)*sin(e2/2)*sin(e1/2) + sin(e3/2)*cos(e2/2)*cos(e1/2);
 
     quaternion(1,1) = q0;
     quaternion(2,1) = q1;
