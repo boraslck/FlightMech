b4083c60080729a17ba751cb8301d9a5c940ee48
boraslck <bsel0779@uni.sydney.edu.au>
2022-10-18 14:56:59 +1100

    A

---
+++ Main.m
@@ -0,0 +1,28 @@
+%% Main
+
+%%% MAIN
+clf;
+clf reset;
+close all;
+clear;
+clc;
+
+% Main Simulation Script
+run Control_GUI
+clc
+
+%% Setup
+
+% Choose CG and Velocity
+cg_pos      = 1;            % Nominal (1), Secondary (2)
+V           = 100;          % Velocity (180), (100)
+
+% Simulation Parameters
+dt          = 
+
+
+
+
+
+
+
