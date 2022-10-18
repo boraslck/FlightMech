52683d511b8d86c58e8d22988e8aeb4da580fd47
damowaztaken <104478173+damowaztaken@users.noreply.github.com>
2022-10-18 15:05:54 +1100

    changes

--- Functions/StateRates.m
+++ Functions/StateRates.m
@@ -72,16 +72,7 @@ function [Xdot, CL, Fa_y] = StateRates(FlightData, X, U, angle_rates)
     q3dot = -0.5.*(q2.*p - q1.*q - q0.*r);
     
     % Transformation Matrices
-    l1 = q0^2 + q1^2 - q2^2 - q3^2;
-    l2 = 2*(q1*q2 + q0*q3);
-    l3 = 2*(q1*q3 - q0*q2);
-    m1 = 2*(q1*q2 - q0*q3);
-    m2 = q0^2 - q1^2 + q2^2 - q3^2;
-    m3 = 2*(q2*q3 + q0*q1);
-    n1 = 2*(q0*q2 + q1*q3);
-    n2 = 2*(q2*q3 - q0*q1); 
-    n3 = q0^2 - q1^2 - q2^2 + q3^2;
-    Cbe = [l1 l2 l3; m1 m2 m3; n1 n2 n3];
+    Cbe = DCM(X);
     Ceb = Cbe';
 
     % Position Rates
