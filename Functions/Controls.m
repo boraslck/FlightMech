00fa5b25fe231dbebaca2d793c51fa08a456a09f
boraslck <bsel0779@uni.sydney.edu.au>
2022-10-21 10:20:10 +1100

    controls

--- Functions/Controls.m
+++ Functions/Controls.m
@@ -1,6 +1,37 @@
-function [delta_t,delta_e,delta_a,delta_r] = Controls(delta_td,delta_ed,delta_ad,delta_rd)
-    delta_t = deg2rad(delta_td);
-    delta_e = deg2rad(delta_ed);
-    delta_a = deg2rad(delta_ad);
-    delta_r = deg2rad(delta_rd);
+
+% This function finds the controls for a manouver in the simulation
+
+function U_man = Controls(U_Trim, Time, U_filter, T_filter)
+
+U_man = U_Trim;
+
+    if Time <= T_filter(end)
+
+    %         Go through all the timesteps
+        for n = 1:length(T_filter)
+    %             FIX
+            if Time < 1.05*T_filter(n) && Time > 0.95*T_filter(n)
+                break
+            end
+
+        end
+            
+
+            change  = (any(U_filter(1:4,:),2) ~= 0);
+            
+
+            % Change the input function accordingly
+            U_filter = U_filter(1:4,n);
+            U_man(change) = U_filter(change);
+            return
+        else
+    U_man = U_Trim;
+
+    end
 end
+    
+
+
+
+%     
+%         
--- Main.m
+++ Main.m
@@ -30,7 +30,7 @@ n_pt        = tf/dt;            % Number of Intervals
 X           = zeros(13,n_pt);   % State Vector
 U           = zeros(4,n_pt);    % Control Vector
 T           = zeros(1,n_pt);    % Time Vector
-Time        = dt:dt:tf;          % Points in Time
+T           = t0:dt:tf;         % Points in Time
 %% Initialise
 % Specify the Flight Data
 [X0_init U0_init FlightData] = Initialise(V,cg_pos);
@@ -42,14 +42,38 @@ U0          = U0_init;
 
 %% Trim Routine
 [X_trim U_trim] = Trim (FlightData,X0);
-
+X = X_trim;
+U = U_trim;
 
 %% Integrate
-for i = 1:n_pt
-[X_new] = Integrate(FlightData,X_trim,U_trim,dt);
-X(:,i) = X_new;
-X_trim = X_new;
-%U(:,i) = U_new;
+% for i = 1:n_pt
+%     
+% [X_new] = Integrate(FlightData,X_trim,U_trim,dt);
+% X(:,i) = X_new;
+% X_trim = X_new;
+% %U(:,i) = U_new;
+% end
+fprintf('Load control file to GUI and output to workspace to continue')
+pause
+clc
+
+for i = 2:length(T)
+    if T(i) <= 1
+        
+        [X_new] = Integrate(FlightData,X(:,i-1),U,dt);
+        X(:,i) = X_new;
+        U(:,i) = U_trim;
+        
+    else
+        
+        U_man = Controls(U_trim,T(i),U_filter,T_filter);
+        
+        [X_new] = Integrate(FlightData,X(:,i-1),U_man,dt);
+        X(:,i) = X_new;
+        U(:,i) = U_man;
+        
+    end
+    
 end
 
 %% Quaternions to Euler Angles
@@ -75,9 +99,9 @@ hold off
     box on
     grid on
     set(gca,'FontSize',18,'FontName','Times New Roman');
-    plot(Time,Eulers(1,:), 'linewidth', 2)
-    plot(Time,Eulers(2,:), 'linewidth', 2)
-    plot(Time,Eulers(3,:), 'linewidth', 2)
+    plot(T,Eulers(1,:), 'linewidth', 2)
+    plot(T,Eulers(2,:), 'linewidth', 2)
+    plot(T,Eulers(3,:), 'linewidth', 2)
     xlabel('Time (seconds)');
     ylabel('Euler Angles ({\circ})');
     legend('Roll $\phi$', 'Pitch $\theta$', 'Yaw $\psi$','interpreter','latex', 'location', 'best');
@@ -89,7 +113,7 @@ hold off
     box on
     grid on
     set(gca,'FontSize',18,'FontName','Times New Roman');
-    plot(Time,Control, 'linewidth', 2);
+    plot(T,Control, 'linewidth', 2);
     xlabel('Time (seconds)');
     ylabel('Control Deflections ($^\circ$)', 'interpreter', 'latex');
     legend('$\delta_t$','$\delta_e$','$\delta_a$','$\delta_r$','interpreter','latex', 'location', 'best');
@@ -101,9 +125,9 @@ hold off
     box on
     grid on
     set(gca,'FontSize',18,'FontName','Times New Roman');
-    plot(Time,X(1,:),'linewidth',2);
-    plot(Time,X(2,:),'linewidth',2);
-    plot(Time,X(3,:),'linewidth',2);
+    plot(T,X(1,:),'linewidth',2);
+    plot(T,X(2,:),'linewidth',2);
+    plot(T,X(3,:),'linewidth',2);
     xlabel('Time (seconds)')
     ylabel('Velocity (m/s)')
     legend('$u$', '$v$', '$w$', 'interpreter','latex', 'location', 'best');
@@ -115,9 +139,9 @@ hold off
     box on
     grid on
     set(gca,'FontSize',18,'FontName','Times New Roman');
-    plot(Time,X(4,:),'linewidth',2);
-    plot(Time,X(5,:),'linewidth',2);
-    plot(Time,X(6,:),'linewidth',2);
+    plot(T,X(4,:),'linewidth',2);
+    plot(T,X(5,:),'linewidth',2);
+    plot(T,X(6,:),'linewidth',2);
     xlabel('Time (seconds)')
     ylabel('Body Rates (rad/s)')
     legend('$p$', '$q$', '$r$', 'interpreter','latex', 'location', 'best');
@@ -129,9 +153,9 @@ hold off
     box on
     grid on
     set(gca,'FontSize',18,'FontName','Times New Roman');
-    plot(Time,X(11,:),'linewidth',2);
-    plot(Time,X(12,:),'linewidth',2);
-    plot(Time,X(13,:),'linewidth',2);
+    plot(T,X(11,:),'linewidth',2);
+    plot(T,X(12,:),'linewidth',2);
+    plot(T,X(13,:),'linewidth',2);
     xlabel('Time (seconds)')
     ylabel('Positions (m)')
     legend('$x_e$','$y_e$', '$z_e$', 'interpreter','latex', 'location', 'best');
