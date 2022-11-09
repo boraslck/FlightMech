b692e34c9bba039ca49e2e5fd408b4a9da3a3d4a
sascaz <115748590+sascaz@users.noreply.github.com>
2022-11-09 13:01:54 +1100

    added CL and an extra input for velocity

--- estimate_angle.m
+++ estimate_angle.m
@@ -1,12 +1,18 @@
-% Quick function to find Da Dr and Beta
-function [da dr beta] = estimate_angle(FlightData,phi)
+% Quick function to estimate Da Dr and Beta from FlightData, phi and
+% velocity
+function [da dr beta] = estimate_angle(FlightData,phi,V)
 
 % Unpack Flight Data to get coefficients
 Cyb = FlightData.Aero.Cyb; Cyda = FlightData.Aero.Cyda; Cydr = FlightData.Aero.Cydr;
 Clb = FlightData.Aero.Clb; Clda = FlightData.Aero.Clda; Cldr = FlightData.Aero.Cldr;
 Cnb = FlightData.Aero.Cnb; Cnda = FlightData.Aero.Cnda; Cndr = FlightData.Aero.Cndr;
 
-% Get CL from lift equation
+% Get CL as per normal Week 7a pg 6
+g = FlightData.Inertial.g; %acceleration due to gravity [m/s^2]
+m = FlightData.Inertial.m; %aircraft mass [kg]
+S = FlightData.Geo.S; %wing planform area [m^2]
+rho = 1.225; %air density [kg/m^3]
+CL = (m*g)/((1/2)*rho*(V^2)*S);
 
 phi = deg2rad(phi); %phi or bank angle presumed to be inputed in degrees
 
